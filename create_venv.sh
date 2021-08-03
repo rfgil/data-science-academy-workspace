613cb1fe97bc17e728fc9f1172ff6d4d2eea815c
rfgil <rafael.gil@talkdesk.com>
2021-08-03 21:48:31 +0100

    Add helper sh to create python virtual envs

---
+++ create_venv.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+venv_dir="/Users/rafael.gil/.virtualenvs/$1"
+
+if [ ! -d "$venv_dir" ]; then
+  /usr/local/opt/python@3.7/bin/python3.7 -m venv $venv_dir
+  echo "Created enviornment: $venv_dir"
+fi
+
+source "$venv_dir/bin/activate"
