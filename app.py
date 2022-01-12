5855adc71daab5cbc11b587c3b5266a4633616a4
rfgil <rafael.gil@talkdesk.com>
2022-01-12 17:50:26 +0000

    Update endpoint for BLU14

--- app.py
+++ app.py
@@ -11,47 +11,6 @@ from peewee import (
 from playhouse.shortcuts import model_to_dict
 from playhouse.db_url import connect
 
-
-########################################
-# Begin database stuff
-
-# the connect function checks if there is a DATABASE_URL env var
-# if it exists, it uses it to connect to a remote postgres db
-# otherwise, it connects to a local sqlite db stored in predictions.db
-DB = connect(os.environ.get('DATABASE_URL') or 'sqlite:///predictions.db')
-
-class Prediction(Model):
-    observation_id = IntegerField(unique=True)
-    observation = TextField()
-    proba = FloatField()
-    true_class = IntegerField(null=True)
-
-    class Meta:
-        database = DB
-
-
-DB.create_tables([Prediction], safe=True)
-
-# End database stuff
-########################################
-
-########################################
-# Unpickle the previously-trained model
-
-
-with open('columns.json') as fh:
-    columns = json.load(fh)
-
-pipeline = joblib.load('pipeline.pickle')
-
-with open('dtypes.pickle', 'rb') as fh:
-    dtypes = pickle.load(fh)
-
-
-# End model un-pickling
-########################################
-
-
 ########################################
 # Begin webserver stuff
 
@@ -60,29 +19,79 @@ app = Flask(__name__)
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    response = {'proba': float(1)}
-    return jsonify(response)
+    # YOUR CODE HERE
+    response = {}
 
-
-@app.route('/update', methods=['POST'])
-def update():
-    obs = request.get_json()
     try:
-        p = Prediction.get(Prediction.observation_id == obs['id'])
-        p.true_class = obs['true_class']
-        p.save()
-        return jsonify(model_to_dict(p))
-    except Prediction.DoesNotExist:
-        error_msg = 'Observation ID: "{}" does not exist'.format(obs['id'])
-        return jsonify({'error': error_msg})
+        response["observation_id"] = request["observation_id"]
+    except:
+        response["observation_id"] = None
+        response["error"] = "Unavailable observation_id"
+        return response
+
+    # Retrieve request columns from 'data'
+    try:
+        request_columns = list(request["data"].keys())
+    except:
+        response["error"] = "data does not exist"
+        return response
 
 
-@app.route('/list-db-contents')
-def list_db_contents():
-    return jsonify([
-        model_to_dict(obs) for obs in Prediction.select()
-    ])
+    # Check all requested columns are valid
+    valid_columns = ["age", "sex", "race", "workclass", "education", "marital-status", "capital-gain", "capital-loss", "hours-per-week"]
+    for column in request_columns:
+        if (column != "observation_id" and
+            column != "probability" and
+            column != "prediction" and
+            column not in valid_columns):
+            response["error"] = column + " is not a valid column"
+            return response
 
+    # Check if there are missing columns
+    for column in valid_columns:
+        if(column not in request_columns):
+            response["error"] = column + " column is missing"
+            return response
+
+    # Check if 'sex' column matches expected input
+    if request["data"]["sex"].lower() not in ['male', 'female'] :
+        response["error"] = "Unexpected " + request["data"]["sex"] + " category on 'sex' column"
+        return response
+
+    # Check if 'race' column matches expected input
+    if request["data"]["race"].lower() not in ['white', 'black', 'asian-pac-islander', 'amer-indian-eskimo', 'other']:
+        response["error"] = "Unexpected " + request["data"]["race"] + " category on 'race' column"
+        return response
+
+    # Check if 'age' column matches expected input
+    age = int(request["data"]["age"])
+    if age < 0 or age > 100:
+        response["error"] = "Unexpected 'age' of " + str(age) + " years old"
+        return response
+
+    # Check if 'capital-gain' column matches expected input
+    capital_gains = float(request["data"]["capital-gain"])
+    if capital_gains < 0: # or capital_gains > 99999:
+        response["error"] = "Column 'capital-gain' of " + str(capital_gains) + " exceed bounds."
+        return response
+
+    # Check if 'capital-loss' column matches expected input
+    capital_loss = float(request["data"]["capital-loss"])
+    if capital_loss < 0:# or capital_loss > 4356:
+        response["error"] = "Column 'capital-loss' of " + str(capital_loss) + " exceed bounds."
+        return response
+
+    # Check if 'hours-per-week' column matches expected input
+    hours_per_week = int(request["data"]["hours-per-week"])
+    if hours_per_week < 0 or hours_per_week > 168:
+        response["error"] = "Column 'hours-per-week' of " + str(hours_per_week) + " exceed bounds."
+        return response
+
+    predict_response = predict(request)
+    response["prediction"] = True #predict_response["prediction"]
+    response["probability"] = float(1) #predict_response["probability"]
+
+    return response
 
 # End webserver stuff
 ########################################
